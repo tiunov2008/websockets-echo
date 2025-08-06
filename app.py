76c1ce0e6a4e20b422033b511a4dd9551d9edfdb
Tiunov Vladimir <tiunov2008.va@gmail.com>
2025-08-07 01:36:04 +0300

    fix

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
+
 import asyncio
-import signal
 import http
+import os
+import signal
 
 from websockets.asyncio.server import serve
+
 
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
+
 
 def health_check(connection, request):
     if request.path == "/healthz":
@@ -20,13 +25,13 @@ async def main():
         def on_stop():
             stop.set()
 
-        # On Unix, catch SIGTERM and SIGINT
         loop = asyncio.get_running_loop()
         if hasattr(signal, "SIGTERM"):
             loop.add_signal_handler(signal.SIGTERM, on_stop)
         loop.add_signal_handler(signal.SIGINT, on_stop)
 
         await stop.wait()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
