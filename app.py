8c26fcbd8cddc15a66333d865092e70252c56046
Tiunov Vladimir <tiunov2008.va@gmail.com>
2025-08-07 01:40:37 +0300

    faw

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 import asyncio
 import http
-import os
 import signal
 
 from websockets.asyncio.server import serve
@@ -17,20 +16,12 @@ def health_check(connection, request):
     if request.path == "/healthz":
         return connection.respond(http.HTTPStatus.OK, "OK\n")
 
+
 async def main():
-    port = 8000
-    async with serve(echo, "", port, process_request=health_check) as server:
-        stop = asyncio.Event()
-
-        def on_stop():
-            stop.set()
-
+    async with serve(echo, "", 8080, process_request=health_check) as server:
         loop = asyncio.get_running_loop()
-        if hasattr(signal, "SIGTERM"):
-            loop.add_signal_handler(signal.SIGTERM, on_stop)
-        loop.add_signal_handler(signal.SIGINT, on_stop)
-
-        await stop.wait()
+        loop.add_signal_handler(signal.SIGTERM, server.close)
+        await server.wait_closed()
 
 
 if __name__ == "__main__":
